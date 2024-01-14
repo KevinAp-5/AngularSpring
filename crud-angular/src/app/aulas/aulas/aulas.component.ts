e3dd0af14770196c1f20aa4cbb21ec6e851a873b
KevinAp-5 <keven.clash3@gmail.com>
2024-01-14 01:36:17 -0300

    fixed sintax errors

--- crud-angular/src/app/aulas/aulas/aulas.component.ts
+++ crud-angular/src/app/aulas/aulas/aulas.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {Aula} from "../model/aula";
-import { AulasService } from '../services/aulas.service';
-import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, of } from 'rxjs';
 import { MatDialog } from '@angular/material/dialog';
+import { Observable, catchError, of } from 'rxjs';
 import { ErrorDialogComponent } from '../../shared/components/error-dialog/error-dialog.component';
+import { Aula } from "../model/aula";
+import { AulasService } from '../services/aulas.service';
 
 
 @Component({
@@ -26,26 +25,25 @@ export class AulasComponent implements OnInit{
     private aulasService: AulasService,
     public dialog: MatDialog
     ) {
-    //this.aulasService = new AulasService();
     this.aulas$ = this.aulasService.list().pipe(
       catchError(error => {
         this.OnError("Erro ao carregar cursos. Tente novamente mais tarde");
         return of([])
       })
     );
+    }
 
+    OnAdd(): void{
+      console.log('teste');
+    }
 
-  OnAdd(): void{
-    console.log('teste');
-  }
+    OnError(ErroMensagem: string) {
+      this.dialog.open(ErrorDialogComponent, {
+        data: ErroMensagem
+      });
+    }
 
-  OnError(ErroMensagem: string) {
-    this.dialog.open(ErrorDialogComponent, {
-      data: ErroMensagem
-    });
-  }
-
-  ngOnInit(): void {
-
+    ngOnInit(): void {
+      //
   }
 }
