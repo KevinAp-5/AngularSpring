59b6c3b4709440a493adf27f79a9e05db0d78217
KevinAp-5 <keven.clash3@gmail.com>
2024-01-14 01:20:53 -0300

    aula-form

---
+++ crud-angular/src/app/aulas/aula-form/aula-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aula-form',
+  templateUrl: './aula-form.component.html',
+  styleUrl: './aula-form.component.scss'
+})
+export class AulaFormComponent {
+
+}
